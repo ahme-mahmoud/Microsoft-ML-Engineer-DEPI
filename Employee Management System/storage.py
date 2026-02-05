fbae400df32d5b9271bf40d1157b5ecdf76b6702
Ahmed Mahmoud <ahmedmahmoud9653@gmail.com>
2026-02-05 21:42:21 +0200

    Update storage.py

--- Employee Management System/storage.py
+++ Employee Management System/storage.py
@@ -1,1 +1,21 @@
 
+import csv, os
+
+FILE = "employees.csv"
+FIELDS = ["ID", "Name", "Position", "Salary", "Email"]
+
+class Storage:
+    def __init__(self):
+        self.data = self.load()
+
+    def load(self):
+        if not os.path.exists(FILE):
+            return {}
+        with open(FILE) as f:
+            return {row["ID"]: row for row in csv.DictReader(f)}
+
+    def save(self):
+        with open(FILE, "w", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=FIELDS)
+            writer.writeheader()
+            writer.writerows(self.data.values())
