586c11c709090406c557be41a7ed5612f77c8cd3
Ahmed Mahmoud <ahmedmahmoud9653@gmail.com>
2026-02-05 21:41:19 +0200

    Update model.py

--- Employee Management System/model.py
+++ Employee Management System/model.py
@@ -1,1 +1,21 @@
+import re
+
+class Employee:
+    def __init__(self, emp_id, name, position, salary, email):
+        self.id = emp_id
+        self.name = name
+        self.position = position
+        self.salary = salary
+        self.email = email
+
+class Validator:
+    @staticmethod
+    def validate(data):
+        if not data["ID"].isdigit():
+            return False, "ID must be a number"
+        if float(data["Salary"]) <= 0:
+            return False, "Salary must be positive"
+        if data["Email"] and not re.match(r"[^@]+@[^@]+\.[^@]+", data["Email"]):
+            return False, "Invalid email"
+        return True, ""
 
