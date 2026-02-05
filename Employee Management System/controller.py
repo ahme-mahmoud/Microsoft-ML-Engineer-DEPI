fd95f9400f3e417facb09973914982a60a35e2c3
Ahmed Mahmoud <ahmedmahmoud9653@gmail.com>
2026-02-05 21:44:54 +0200

    Update controller.py

--- Employee Management System/controller.py
+++ Employee Management System/controller.py
@@ -1,1 +1,25 @@
 
+from model import Validator
+from storage import Storage
+
+class Controller:
+    def __init__(self):
+        self.store = Storage()
+
+    def add(self, emp):
+        valid, msg = Validator.validate(emp)
+        if not valid:
+            return False, msg
+        self.store.data[emp["ID"]] = emp
+        self.store.save()
+        return True, "Added successfully"
+
+    def delete(self, emp_id):
+        if emp_id in self.store.data:
+            del self.store.data[emp_id]
+            self.store.save()
+            return True
+        return False
+
+    def all(self):
+        return list(self.store.data.values())
